7ff732ef583dddb0f8d0db18df9879bb4e6e99ba
cowboy8625 <cowboy8625@yahoo.com>
2023-09-01 06:15:18 -0500

    neovim to the party

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,23 @@
+function remove_numbers()
+  vim.wo.number = not vim.wo.number
+  vim.wo.relativenumber = not vim.wo.relativenumber
+end
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('t', '<c-k>', '<c-\\><c-n>:wincmd k<CR>')
+vim.keymap.set('t', '<c-j>', '<c-\\><c-n>:wincmd j<CR>')
+vim.keymap.set('t', '<c-h>', '<c-\\><c-n>:wincmd h<CR>')
+vim.keymap.set('t', '<c-l>', '<c-\\><c-n>:wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>nln', remove_numbers)
+
+
+-- vim.keymap.set('n', '<A-k>', ':m -2<CR>')
+-- vim.keymap.set('n', '<A-j>', ':m +1<CR>')
+
